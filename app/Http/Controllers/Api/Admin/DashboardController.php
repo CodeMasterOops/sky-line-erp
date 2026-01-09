0dbfce422629c8f7520f08975c80cd88da8f2927
Manoj Oli <manoj@futuressoft.com>
2026-01-09 10:42:57 +0545

    dashboard update info box

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Models\User;
+use App\Models\Product;
+use App\Models\Warehouse;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
@@ -10,6 +12,9 @@ class DashboardController extends Controller
     public function __invoke()
     {
         return response()->json([
+            'customers_count' => 0,
+            'products_count' => Product::count(),
+            'warehouse_count' => Warehouse::count(),
             'users_count' => User::count(),
         ]);
     }
--- resources/js/views/admin/Dashboard.vue
+++ resources/js/views/admin/Dashboard.vue
@@ -1,32 +1,65 @@
 <template>
-  <section class="section dashboard mt-2">
-    <div class="row">
-      <div class="col-md-3 col-6">
-        <div class="info-box">
-          <span class="info-box-icon bg-info elevation-1 text-light">
-            <i class="fa fa-user"></i>
-          </span>
-          <div class="info-box-content">
-            <span class="info-box-text">Users</span>
-            <span class="info-box-number">{{ dashboard.data.users_count }}</span>
-          </div>
+    <section class="section dashboard mt-2">
+        <div class="row">
+            <div class="col-md-3 col-6">
+                <div class="info-box">
+                      <span class="info-box-icon bg-info elevation-1 text-light">
+                        <i class="fa fa-users"></i>
+                      </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Customers</span>
+                        <span class="info-box-number">{{ dashboard.data.customers_count }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="info-box">
+                  <span class="info-box-icon bg-secondary elevation-1 text-light">
+                    <i class="fa fa-product-hunt"></i>
+                  </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Products</span>
+                        <span class="info-box-number">{{ dashboard.data.products_count }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="info-box">
+                  <span class="info-box-icon bg-success elevation-1 text-light">
+                    <i class="fa fa-building-o"></i>
+                  </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Warehouse</span>
+                        <span class="info-box-number">{{ dashboard.data.warehouse_count }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="info-box">
+                  <span class="info-box-icon bg-primary elevation-1 text-light">
+                    <i class="fa fa-user"></i>
+                  </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Users</span>
+                        <span class="info-box-number">{{ dashboard.data.users_count }}</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script setup>
-import {onMounted} from "vue";
-import {useAdminDashboardStore} from "@/stores/admin/dashboard";
-import {storeToRefs} from "pinia";
+import { onMounted } from 'vue';
+import { useAdminDashboardStore } from '@/stores/admin/dashboard';
+import { storeToRefs } from 'pinia';
 
 const dashboardStore = useAdminDashboardStore();
 
 onMounted(() => {
-  dashboardStore.getDashboardData();
-})
+    dashboardStore.getDashboardData();
+});
 
-const {dashboard} = storeToRefs(dashboardStore);
+const { dashboard } = storeToRefs(dashboardStore);
 
 </script>
