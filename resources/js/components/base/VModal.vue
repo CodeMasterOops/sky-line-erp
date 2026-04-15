dd48df1f00afa16f7e3b327bb10785bc500479f6
CodeMasterOops <mypari977@gmail.com>
2026-04-15 18:18:32 +0545

    Remove modal component styles and references from SCSS and Vue files, streamlining the UI by eliminating unused modal functionality.

--- resources/js/components/base/VModal.vue
+++ resources/js/components/base/VModal.vue
@@ -1,47 +1,30 @@
 <template>
     <Transition name="modal">
-        <div
-            v-if="showModal"
-            class="modal fade show d-block"
-            tabindex="-1"
-            role="dialog"
-            style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1050; overflow-x: hidden; overflow-y: auto;"
-        >
+        <div v-if="showModal" class="modal fade show d-block" tabindex="-1" role="dialog"
+            style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1050; overflow-x: hidden; overflow-y: auto;">
             <!-- Backdrop -->
             <div class="modal-backdrop fade show" @click="handleBackdropClick"></div>
 
-            <div
-                class="modal-dialog modal-dialog-centered"
-                :class="[sizeClass, modalClass]"
-                role="document"
-                @click.stop
-                style="position: relative; z-index: 1055;"
-            >
+            <div class="modal-dialog modal-dialog-centered" :class="[sizeClass, modalClass]" role="document" @click.stop
+                style="position: relative; z-index: 1055;">
                 <div class="modal-content">
                     <div class="page-wrapper-new p-0">
-                        <div class="content">
-                            <!-- Header -->
-                            <div class="modal-header border-0 custom-modal-header">
-                                <slot name="header">
-                                    <div class="page-title">
-                                        <h4>{{ title }}</h4>
-                                    </div>
-                                </slot>
+                        <!-- Header -->
+                        <div class="modal-header border-0 custom-modal-header">
+                            <slot name="header">
+                                <div class="page-title">
+                                    <h4>{{ title }}</h4>
+                                </div>
+                            </slot>
 
-                                <button
-                                    type="button"
-                                    class="close"
-                                    aria-label="Close"
-                                    @click="$emit('closeClick')"
-                                >
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
+                            <button type="button" class="close" aria-label="Close" @click="$emit('closeClick')">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
 
-                            <!-- Body -->
-                            <div class="modal-body custom-modal-body">
-                                <slot name="modal-body"/>
-                            </div>
+                        <!-- Body -->
+                        <div class="modal-body custom-modal-body">
+                            <slot name="modal-body" />
                         </div>
                     </div>
                 </div>
@@ -102,5 +85,5 @@ watch(() => props.showModal, (isOpen) => {
     } else {
         document.body.style.overflow = ''
     }
-}, {immediate: true})
+}, { immediate: true })
 </script>
